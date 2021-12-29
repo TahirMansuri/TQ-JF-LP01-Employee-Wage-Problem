8c478d71f7e07a61dabd742d92bbea38f1346642
TahirMansuri <tprocks786@gmail.com>
2021-12-29 11:34:14 +0530

    ADDED: Employee Wages Problem

---
+++ src/com/bridgelabz/employeewagesproblem/EmployeeWages.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewagesproblem;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+
+        //Welcome Message for Initial Purpose
+        System.out.println("Welcome to Employee Wages Problem Developed by Tahir Mansuri.");
+    }
+}
